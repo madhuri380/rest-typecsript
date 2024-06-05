db465f5bb67165d4af923d7619d36aefa4f36d5a
madhuri380 <madhuria380@gmail.com>
2024-06-04 19:29:00 -0700

    adding output

--- src/unit-test-examples/units.tests.ts
+++ src/unit-test-examples/units.tests.ts
@@ -1,4 +1,4 @@
-import { add, divide } from './units';
+import { add, divide, concat } from './units';
 
 import { expect } from 'chai';
 import 'mocha';
@@ -38,3 +38,25 @@ describe('divide', () => {
 // @TODO try creating a new describe block for the "concat" method
 // it should contain an it block for each it statement in the units.ts @TODO.
 // don't forget to import the method ;)
+
+describe('concat function', () => {
+
+  it('should concatenate "Hello" and " World"', () => {
+    const result = concat("Hello", " World");
+    expect(result).to.equal("Hello World");
+  });
+
+  it('should throw an error if the first string is empty', () => {
+    expect(() => { concat("", "World") }).to.throw('Both strings must be non-empty');
+  });
+
+  it('should throw an error if the second string is empty', () => {
+    expect(() => { concat("Hello", "") }).to.throw('Both strings must be non-empty');
+  });
+
+  it('should throw an error if both strings are empty', () => {
+    expect(() => { concat("", "") }).to.throw('Both strings must be non-empty');
+  });
+
+});
+
